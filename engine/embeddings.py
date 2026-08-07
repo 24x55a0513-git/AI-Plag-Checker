03525d4bce9dc6b95aedc1e0f062ad4032ba5ca6
Manoj <24x55a0513@srecnandyal.edu.in>
2026-08-07 10:53:07 +0530

    Replace CodeBERT with MiniLM

--- engine/embeddings.py
+++ engine/embeddings.py
@@ -3,33 +3,24 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 model = None
 
-
 def load_model():
     global model
 
     if model is None:
-        print("Loading MiniLM model...")
-        model = SentenceTransformer(
-    "flax-sentence-embeddings/st-codesearch-distilroberta-base"
-)
+        print("Loading MiniLM...")
+        model = SentenceTransformer("all-MiniLM-L6-v2")
         print("MiniLM loaded.")
-
 
 def get_embedding(code):
     load_model()
-    return model.encode([code])
-
+    return model.encode(
+        code,
+        convert_to_numpy=True
+    ).reshape(1, -1)
 
 def embedding_similarity(code1, code2):
-
     emb1 = get_embedding(code1)
     emb2 = get_embedding(code2)
 
-    score = cosine_similarity(
-        emb1,
-        emb2
-    )[0][0]
-
-    score = max(0, min(score, 1))
-
+    score = cosine_similarity(emb1, emb2)[0][0]
     return round(score * 100, 2)
